3a818099c5022d9f152284dff11139ccf710a35c
Lynn <tw781297125@gmail.com>
2019-07-01 15:20:11 +0800

    add build.sh

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -x
+
+SOURCE_DIR=`pwd`
+
+if [ ! -f "build" ]; then
+    mkdir build
+fi
+
+cd build \
+    && cmake $SOURCE_DIR \
+    && make
+
+cd $SOURCE_DIR
+if [ ! -f "bin" ]; then
+    mkdir bin
+fi
+
+cp build/src/webserver bin
